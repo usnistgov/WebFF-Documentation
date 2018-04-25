168b0ed75fb208dbca041e594f7c01d803ed9478
Garcia Beltran <png3@NIST.GOV>
2018-04-25 17:44:03 -0400

    Changes to the code made after testing in addition to folder within the workspace folder containing all testing files (inputs, outputs, etc).

--- Workspace/Testing/Class1_Excel_to_XML.py
+++ Workspace/Testing/Class1_Excel_to_XML.py
@@ -19,7 +19,7 @@ def excel_xml(input, output):
     output_file_path = (output)
 
     # Create the file root
-    root = ET.Element("FF-Class2") 
+    root = ET.Element("FF-Class-I") 
     
     sheet_names = xls_file.sheet_names() 
     #Pick the Specified Potentials
@@ -50,137 +50,111 @@ def excel_xml(input, output):
         FF.ReadExcelAtomTypeAttributes_DFF(sheet, root)
     if  "Atom-Attributes-Generic" in sheet_names: 
         sheet = xls_file.sheet_by_name("Atom-Attributes-Generic")
-        FF.ReadExcelAtomTypeAttributes_Generic(sheet, root)    
-	
+        FF.ReadExcelAtomTypeAttributes_Generic(sheet, root)
+    
     #Bond Potentials 
     if "BondPotential-Harmonic" in sheet_names:
         sub_root = ET.SubElement(root, "BondPotential")
         sheet = xls_file.sheet_by_name("BondPotential-Harmonic")
         FF.ReadExcelBondPotential_Harmonic(sheet,sub_root) 
-    elif "BondPotential-Class2" in sheet_names:
+    if "BondPotential-Morse" in sheet_names:
         sub_root = ET.SubElement(root, "BondPotential")
-        sheet = xls_file.sheet_by_name("BondPotential-Class2")
-        FF.ReadExcelBondPotential_Class2(sheet,sub_root)
+        sheet = xls_file.sheet_by_name("BondPotential-Morse")
+        FF.ReadExcelBondPotential_Morse(sheet,sub_root)
         
-    #Angle Potentials
+    #Angle Potentials   
     if  "AnglePotential-Harmonic" in sheet_names: 
         sub_root = ET.SubElement(root, "AnglePotential")
         sheet = xls_file.sheet_by_name("AnglePotential-Harmonic")
         FF.ReadExcelAnglePotential_Harmonic(sheet,sub_root) 
-    elif  "AnglePotential-COS2" in sheet_names: 
+    if  "AnglePotential-COS2" in sheet_names: 
         sub_root = ET.SubElement(root, "AnglePotential")
         sheet = xls_file.sheet_by_name("AnglePotential-COS2")
         FF.ReadExcelAnglePotential_COS2(sheet, sub_root)
-    elif  "AnglePotential-CHARMM" in sheet_names: 
+    if  "AnglePotential-CHARMM" in sheet_names: 
         sub_root = ET.SubElement(root, "AnglePotential")
         sheet = xls_file.sheet_by_name("AnglePotential-CHARMM")
-        FF.ReadExcelAnglePotential_CHARMM(sheet, sub_root)
-    elif  "AnglePotential-Class2" in sheet_names: 
-		sub_root = ET.SubElement(root, "AnglePotential")
-		sheet = xls_file.sheet_by_name("AnglePotential-Class2")
-		FF.ReadExcelAnglePotential_Class2(sheet, sub_root)
+        FF.ReadExcelAnglePotential_CHARMM(sheet, sub_root) 
+
 
     #Dihedral Potentials    
+    
     if  "DihedralPotential-CHARMM" in sheet_names: 
         sub_root = ET.SubElement(root, "DihedralPotential")
         sheet = xls_file.sheet_by_name("DihedralPotential-CHARMM")
         FF.ReadExcelDihedralPotential_CHARMM(sheet, sub_root) 
-    elif  "DihedralPotential-Harmonic" in sheet_names: 
+    if  "DihedralPotential-Harmonic" in sheet_names: 
         sub_root = ET.SubElement(root, "DihedralPotential")
         sheet = xls_file.sheet_by_name("DihedralPotential-Harmonic")
         FF.ReadExcelDihedralPotential_Harmonic(sheet, sub_root) 
-    elif  "DihedralPotential-Quadratic" in sheet_names: 
+    if  "DihedralPotential-Quadratic" in sheet_names: 
         sub_root = ET.SubElement(root, "DihedralPotential")
         sheet = xls_file.sheet_by_name("DihedralPotential-Quadratic")
         FF.ReadExcelDihedralPotential_Quadratic(sheet, sub_root) 
-    elif  "DihedralPotential-OPLS" in sheet_names: 
+    if  "DihedralPotential-OPLS" in sheet_names: 
         sub_root = ET.SubElement(root, "DihedralPotential")
         sheet = xls_file.sheet_by_name("DihedralPotential-OPLS")
         FF.ReadExcelDihedralPotential_OPLS(sheet, sub_root)  
-    elif "DihedralPotential-FourierSimple" in sheet_names: 
+    if "DihedralPotential-FourierSimple" in sheet_names: 
         sub_root = ET.SubElement(root, "DihedralPotential")
         sheet = xls_file.sheet_by_name("DihedralPotential-FourierSimple")
-        FF.ReadExcelDihedralPotential_FourierSimple(sheet, sub_root)
-    elif "DihedralPotential-Fourier" in sheet_names: 
+        FF.ReadExcelDihedralPotential_FourierSimple(sheet, sub_root) 
+    if "DihedralPotential-Fourier" in sheet_names: 
         sub_root = ET.SubElement(root, "DihedralPotential")
         sheet = xls_file.sheet_by_name("DihedralPotential-Fourier")
-        FF.ReadExcelDihedralPotential_Fourier(sheet, sub_root) 
-    elif "DihedralPotential-Class2" in sheet_names: 
-        sub_root = ET.SubElement(root, "DihedralPotential")
-        sheet = xls_file.sheet_by_name("DihedralPotential-Class2")
-        FF.ReadExcelDihedralPotential_Class2(sheet, sub_root) 
+        FF.ReadExcelDihedralPotential_Fourier(sheet, sub_root)  
 
 
-    # Improper Potentials 
+    # Improper Potentials
+    
     if "ImproperPotential-CVFF" in sheet_names: 
         sub_root = ET.SubElement(root, "ImproperPotential")
         sheet = xls_file.sheet_by_name("ImproperPotential-CVFF")
         FF.ReadExcelImproperPotential_CVFF(sheet, sub_root) 
-    elif  "ImproperPotential-COS2" in sheet_names: 
+    if  "ImproperPotential-COS2" in sheet_names: 
         sub_root = ET.SubElement(root, "ImproperPotential")
         sheet = xls_file.sheet_by_name("ImproperPotential-COS2")
         FF.ReadExcelImproperPotential_COS2(sheet, sub_root) 
-    elif  "ImproperPotential-Harmonic" in sheet_names: 
+    if  "ImproperPotential-Harmonic" in sheet_names: 
         sub_root = ET.SubElement(root, "ImproperPotential")
         sheet = xls_file.sheet_by_name("ImproperPotential-Harmonic")
         FF.ReadExcelImproperPotential_Harmonic(sheet, sub_root) 
-    elif  "ImproperPotential-Fourier" in sheet_names: 
+    if  "ImproperPotential-Fourier" in sheet_names: 
         sub_root = ET.SubElement(root, "ImproperPotential")
         sheet = xls_file.sheet_by_name("ImproperPotential-Fourier")
         FF.ReadExcelImproperPotential_Fourier(sheet, sub_root) 
-    elif  "ImproperPotential-Umbrella" in sheet_names: 
+    if  "ImproperPotential-Umbrella" in sheet_names: 
         sub_root = ET.SubElement(root, "ImproperPotential")
         sheet = xls_file.sheet_by_name("ImproperPotential-Umbrella")
         FF.ReadExcelImproperPotential_Umbrella(sheet, sub_root)  
-    elif  "ImproperPotential-CHARMM" in sheet_names: 
+    if  "ImproperPotential-CHARMM" in sheet_names: 
         sub_root = ET.SubElement(root, "ImproperPotential")
         sheet = xls_file.sheet_by_name("ImproperPotential-CHARMM")
         FF.ReadExcelImproperPotential_CHARMM(sheet, sub_root) 
-    elif  "ImproperPotential-Class2" in sheet_names: 
-        sub_root = ET.SubElement(root, "ImproperPotential")
-        sheet = xls_file.sheet_by_name("ImproperPotential-Class2")
-        FF.ReadExcelImproperPotential_Class2(sheet, sub_root) 
 
-	#Cross Potentials
-    if "CrossPotential-BondBond" in sheet_names: 
-        sub_root = ET.SubElement(root, "CrossPotential")
-        sheet = xls_file.sheet_by_name("CrossPotential-BondBond")
-        FF.ReadExcelCrossPotential_BondBond(sheet, sub_root) 
-    if  "CrossPotential-BondBond13" in sheet_names: 
-        sub_root = ET.SubElement(root, "CrossPotential")
-        sheet = xls_file.sheet_by_name("CrossPotential-BondBond13")
-        FF.ReadExcelCrossPotential_BondBond13(sheet, sub_root) 
-    if  "CrossPotential-AngleAngle" in sheet_names: 
-        sub_root = ET.SubElement(root, "CrossPotential")
-        sheet = xls_file.sheet_by_name("CrossPotential-AngleAngle")
-        FF.ReadExcelCrossPotential_AngleAngle(sheet, sub_root) 
-    if  "CrossPotential-BondAngle" in sheet_names: 
-        sub_root = ET.SubElement(root, "CrossPotential")
-        sheet = xls_file.sheet_by_name("CrossPotential-BondAngle")
-        FF.ReadExcelCrossPotential_BondAngle(sheet, sub_root) 
-    if  "CrossPotential-MiddleBondTorsio" in sheet_names: 
-        sub_root = ET.SubElement(root, "CrossPotential")
-        sheet = xls_file.sheet_by_name("CrossPotential-MiddleBondTorsio")
-        FF.ReadExcelCrossPotential_MiddleBondTorsion(sheet, sub_root)  
-    if  "CrossPotential-EndBondTorsion" in sheet_names: 
-        sub_root = ET.SubElement(root, "CrossPotential")
-        sheet = xls_file.sheet_by_name("CrossPotential-EndBondTorsion")
-        FF.ReadExcelCrossPotential_EndBondTorsion(sheet, sub_root) 
-    if  "CrossPotential-AngleTorsion" in sheet_names: 
-        sub_root = ET.SubElement(root, "CrossPotential")
-        sheet = xls_file.sheet_by_name("CrossPotential-AngleTorsion")
-        FF.ReadExcelCrossPotential_AngleTorsion(sheet, sub_root)
-    if  "CrossPotential-AngleAngleTorsio" in sheet_names: 
-        sub_root = ET.SubElement(root, "CrossPotential")
-        sheet = xls_file.sheet_by_name("CrossPotential-AngleAngleTorsio")
-        FF.ReadExcelCrossPotential_AngleAngleTorsion(sheet, sub_root)
-		
-    #Non Bonded Potentials 
-    if "NonBondPotential-Class2" in sheet_names: 
+    #Non Bonded Potentials
+    
+    if  "NonBondPotential-LJ" in sheet_names: 
         sub_root = ET.SubElement(root, "NonBondPotential")
-        sheet = xls_file.sheet_by_name("NonBondPotential-Class2")
-        FF.ReadExcelNonBondPotential_LJClass2(sheet, sub_root)
-	
+        sheet = xls_file.sheet_by_name("NonBondPotential-LJ")
+        FF.ReadExcelNonBondPotential_LJ(sheet, sub_root)  
+    if  "NonBondPotential-LJ-Rmin" in sheet_names: 
+        sub_root = ET.SubElement(root, "NonBondPotential")
+        sheet = xls_file.sheet_by_name("NonBondPotential-LJ-Rmin")
+        FF.ReadExcelNonBondPotential_LJRmin(sheet, sub_root)  
+    if  "NonBondPotential-LJ-AB" in sheet_names: 
+        sub_root = ET.SubElement(root, "NonBondPotential")
+        sheet = xls_file.sheet_by_name("NonBondPotential-LJ-AB")
+        FF.ReadExcelNonBondPotential_LJAB(sheet, sub_root)  
+    if "NonBondPotential-LJ-96" in sheet_names: 
+        sub_root = ET.SubElement(root, "NonBondPotential")
+        sheet = xls_file.sheet_by_name("NonBondPotential-LJ-96")
+        FF.ReadExcelNonBondPotential_LJ96(sheet, sub_root)
+    if  "NonBondPotential-LJ2" in sheet_names: 
+        sub_root = ET.SubElement(root, "NonBondPotential")
+        sheet = xls_file.sheet_by_name("NonBondPotential-LJ2")
+        FF.ReadExcelNonBondPotential_LJ2(sheet, sub_root)  
+        
     #Other information     
     if  "BondIncrements" in sheet_names: 
         sub_root = ET.SubElement(root, "BondIncrementTable")
