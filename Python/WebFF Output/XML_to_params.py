92e4eb358cc47667890788a176aa6fd6ed53e46e
lizoodharo <alo@NIST.GOV>
2019-04-16 16:09:34 -0400

    Updated params output code

--- Python/WebFF Output/XML_to_params.py
+++ Python/WebFF Output/XML_to_params.py
@@ -1,36 +1,52 @@
-#Importing libraies and modules
+#Importing libraries and modules
 import xml.etree.ElementTree as ET
 import sys                                # Python standard library
-import webff as FF                        # Webff module
+import WebFF as FF                        # Webff module
 #Calls functions from the webff.py module to translate XML data into the .params format
 def xml_params(input, output): 
-    #parses the XML tree and estabishes the root
+    #parses the XML tree and establishes the root
     tree = ET.parse(input)
     root = tree.getroot() 
-    #Opend the output file
+    #Open the output file
     f = open(output, 'w+') 
     #creates a  list of all top level elements (potential types)
     top_elements= (root.findall("*"))
     tags_elements = []
     for ele in top_elements:
         tags_elements.append(ele.tag)
-    f.write("* "+((root.find("./Force-Field-Header/Force-Field-Name")).text)+"\n")
+    f.write("* "+str((root.find("./Force-Field-Header/Force-Field-Name")).text)+"\n")
     #If the potential style is present calls the appropriate webff.py function on it
+	#Non Bond Potentials
     if "NonBondPotential" in tags_elements:
         if root.find("NonBondPotential/*").tag == "NonBond-LJ-Rmin": 
             FF.XMLToParamsNonBondPotential_LJ_Rmin(root, f)
+        if root.find("NonBondPotential/*").tag == "NonBond-LJ": 
+            FF.XMLToParamsNonBondPotential_LJ(root, f)
+	#Bond Potentials
     if "BondPotential" in tags_elements:
         if root.find("BondPotential/*").tag == "BondPotential-Harmonic":
             FF.XMLToParamsBondPotential_Harmonic(root, f)
+        if root.find("BondPotential/*").tag == "BondPotential-Morse":
+            FF.XMLToParamsBondPotential_Morse(root, f)
+	#Angle Potentials
     if "AnglePotential" in tags_elements:
         if root.find("AnglePotential/*").tag == "AnglePotential-Harmonic": 
             FF.XMLToParamsAnglePotential_Harmonic(root, f)
+        if root.find("AnglePotential/*").tag == "AnglePotential-COS2": 
+            FF.XMLToParamsAnglePotential_COS2(root, f)
+	#Dihedral Potentials
     if "DihedralPotential" in tags_elements:
         if root.find("DihedralPotential/*").tag == "DihedralPotential-CHARMM": 
             FF.XMLToParamsDihedralPotential_CHARMM(root, f)
+        if root.find("DihedralPotential/*").tag == "DihedralPotential-FourierSimple": 
+            FF.XMLToParamsDihedralPotential_FourierSimple(root, f)
+	#Improper Potentials
     if "ImproperPotential" in tags_elements:
         if root.find("ImproperPotential/*").tag == "ImproperPotential-Harmonic":
             FF.XMLToParamsImproperPotential_Harmonic(root, f)
+        if root.find("ImproperPotential/*").tag == "ImproperPotential-CHARMM":
+            FF.XMLToParamsImproperPotential_CHARMM(root, f)
+	#Atom Types
     if "AtomTypes" in tags_elements: 
         FF.XMLToParamsAtomTypes(root, f)
     for ele in root.findall("./Force-Field-Header/Attachment"):
