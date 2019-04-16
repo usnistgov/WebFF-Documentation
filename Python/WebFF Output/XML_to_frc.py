409dc583ac47a8ba9d5f1cc973a5bd3d77792830
lizoodharo <alo@NIST.GOV>
2019-04-16 16:09:10 -0400

    Updated frc output code

--- Python/WebFF Output/XML_to_frc.py
+++ Python/WebFF Output/XML_to_frc.py
@@ -1,7 +1,7 @@
-#Importing libraies and modules
+#Importing libraries and modules
 import xml.etree.ElementTree as ET
 import sys                                # Python standard library
-import webff as FF                        # Webff module
+import WebFF as FF                        # Webff module
 #Calls functions from the webff.py module to translate XML data into the .frc format
 def xml_frc(input, output): 
     #parses the XMl tree and establishes the root
@@ -16,26 +16,43 @@ def xml_frc(input, output):
     tags_elements = []
     for ele in top_elements:
         tags_elements.append(ele.tag)
-    #Calls the appropratie webff.py function for each potential style present
+    #Calls the appropriate webff.py function for each potential style present
+	#Atom Types
     if "AtomTypes" in tags_elements: 
         FF.XMLtoFrcAtomTypes(root, f)
+	#Equivalence Tables
     if "EquivalenceTable" in tags_elements: 
         FF.XMLtoFrcEquivalenceTable(root, f)
+	#Bond Potentials
     if "BondPotential" in tags_elements:
         if root.find("BondPotential/*").tag == "BondPotential-Harmonic": 
             FF.XMLtoFrcBondPotential_Harmonic(root, f)
+        if root.find("BondPotential/*").tag == "BondPotential-Morse": 
+            FF.XMLtoFrcBondPotential_Morse(root, f)
+	#Angle Potentials
     if "AnglePotential" in tags_elements:
         if root.find("AnglePotential/*").tag == "AnglePotential-Harmonic": 
             FF.XMLtoFrcAnglePotential_Harmonic(root, f)
+        if root.find("AnglePotential/*").tag == "AnglePotential-COS2": 
+            FF.XMLtoFrcAnglePotential_COS2(root, f)
+	#Improper Potentials
     if "ImproperPotential" in tags_elements:
         if root.find("ImproperPotential/*").tag == "ImproperPotential-CHARMM": 
             FF.XMLtoFrcImproperPotential_CHARMM(root, f)
+        if root.find("ImproperPotential/*").tag == "ImproperPotential-FourierSimple": 
+            FF.XMLtoFrcImproperPotential_FourierSimple(root, f)
+	#Dihedral Potentials
     if "DihedralPotential" in tags_elements:
         if root.find("DihedralPotential/*").tag == "DihedralPotential-FourierSimple": 
             FF.XMLtoFrcDihedralPotential_FourierSimple(root, f)
+        if root.find("DihedralPotential/*").tag == "DihedralPotential-CHARMM": 
+            FF.XMLtoFrcDihedralPotential_CHARMM(root, f)
+	#Non Bonded Potentials
     if "NonBondPotential" in tags_elements:
         if root.find("NonBondPotential/*").tag == "NonBond-LJ": 
             FF.XMLtoFrcNonBond_LJ(root, f)
+        if root.find("NonBondPotential/*").tag == "NonBond-LJ-Rmin": 
+            FF.XMLToFrcNonBondPotential_LJ_Rmin(root, f)
     if "BondIncrements" in tags_elements: 
         FF.XMLtoFrcBondIncrements(root, f)
     for ele in root.findall("./Force-Field-Header/Attachment"):
